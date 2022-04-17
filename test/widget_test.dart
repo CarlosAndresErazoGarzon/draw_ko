002f33ab750f7a931d1372e0a8a5ae04783c296a
CarlosAndresErazoGarzon <carlos-erazo@javeriana.edu.co>
2022-04-17 04:48:18 -0500

    Add first workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:draw_hangul/main.dart';
+import 'package:draw_ko/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
